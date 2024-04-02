b0e8897edae11fb5797a9661ee19c9a860899aef
PatilDarsh <darshpatil072@gmail.com>
2024-04-02 17:25:14 +0530

    [ADDED]:Darshan: Started With Single Player at Position 0

---
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.infogalaxy.snakeandladder;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		int pos = 0 ;
+		System.out.println("The Position of a Player is :"+pos);
+	}
+
+}
