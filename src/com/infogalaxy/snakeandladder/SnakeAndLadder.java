97cb1a12d52410390b0f4dca576bbf05b1625ce6
PatilDarsh <darshpatil072@gmail.com>
2024-04-02 17:32:03 +0530

    [ADDED]:Darshan : Rolling Dice using Random class

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -5,8 +5,11 @@ import java.util.Random;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+		Random random = new Random() ;
 		int pos = 0 ;
+		int rollDie = random.nextInt(6) + 1 ;
 		System.out.println("The Position of a Player is :"+pos);
+		System.out.println("The Position of a Player is :"+rollDie);
 	}
 
 }
